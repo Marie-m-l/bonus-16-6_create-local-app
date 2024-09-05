583143ba4280f58cbbde6a8c3712a2123478fd53
marie_l <mariemlaqua@gmail.com>
2024-09-05 08:39:14 -0700

    Correct spelling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Confetti from 'react-confetti';
 
 function App() {
   return (
     <div className="App">
+      <Confetti />
       <h1>
         Wow, I'm working with React on my machine!
       </h1>
